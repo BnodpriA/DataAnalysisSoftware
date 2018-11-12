81c8d4e40e698b7f5883d68bf36930c834cf601a
[Binod Chaudhary] <[bunnycdy@gmail.com]>
2018-11-12 23:31:04 +0545

    Created File Dialog to choose file

--- DataAnalysisSoftware/Form1.cs
+++ DataAnalysisSoftware/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace DataAnalysisSoftware
 {
@@ -15,6 +16,38 @@ namespace DataAnalysisSoftware
         public Form1()
         {
             InitializeComponent();
+        }
+        string filePath = @"E:\test.text";
+        /// <summary>
+        /// Create File Dialog to choose the file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dr = openFileDialog1.ShowDialog();
+                if (dr==DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        StreamReader files = new StreamReader("");
+      //  string[] columnNames = File.ReadLines
+        public void LoadData()
+        {
+            int i = 0;
+            foreach (DataGridViewRow row in dgvDataInfo.Rows)
+            {
+
+            }
         }
     }
 }
---
+++ FileProcessing/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"E:\test.txt";
+            if (File.Exists(filePath))
+                ProcessFile(filePath);
+            else
+                Console.WriteLine("Error - File {0} not Found", filePath);
+            Console.ReadKey();
+        }
+
+        static void ProcessFile(String filePath)
+        {
+            string oneLine;
+            int lineNo = 1;
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                Console.WriteLine("*====== File.ReadAllText() method ======");
+                Console.WriteLine("{0}", fileContent);
+                Console.WriteLine("==========================================================");
+
+                string[] fileLines = File.ReadAllLines(filePath);
+                Console.WriteLine("====== File.ReadLines() method =======");
+                foreach (string line in fileLines)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine("============================================================");
+
+                StreamReader myFileReader = new StreamReader(filePath);
+                while ((oneLine = myFileReader.ReadLine()) != null)
+                {
+                    Console.WriteLine("\r\n"+"Line " + lineNo + ":");
+                    string[] words = oneLine.Split(' ');
+                    for (int i = 0; i < words.Length; i++)
+                        Console.Write(words[i]);
+                    lineNo++;
+                }
+
+                myFileReader.Close();
+
+            }
+            catch (IOException ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
