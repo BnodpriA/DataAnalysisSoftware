aed067003f14f7644a1363c4ff300deb6e3e6384
[Binod Chaudhary] <[bunnycdy@gmail.com]>
2018-11-14 23:22:00 +0545

    Created Master Class for backend codes

--- DataAnalysisSoftware/MasterClass.cs
+++ DataAnalysisSoftware/MasterClass.cs
@@ -1,55 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace DataAnalysisSoftware
 {
-    public partial class DataSummary : Form
+    /// <summary>
+    /// Class for controlling backed of the DataAnalysisSoftware
+    /// </summary>
+    public class MasterClass
     {
-        public DataSummary()
-        {
-            InitializeComponent();
-        }
-
-        private void DataSummary_Load(object sender, EventArgs e)
-        {
-            cmbSpeedUnit.SelectedIndex = 0; //Set the initial unit of the speed
-        }
         /// <summary>
-        /// Load Serial Number
-        /// </summary>
-        private void LoadSerialNumber()
-        {
-            int i = 1;
-            foreach (DataGridViewRow row in dgvHRMData.Rows)
-            {
-                row.Cells["SNo"].Value = i;
-                i++;
-                i.ToString();
-            }
-        }
-        /// <summary>
-        /// Display Date in Understandable formate i.e. 20130205 to 2013/02/05 (YYYY/MM/DD) Format
-        /// </summary>
-        private void ManageDateFormate()
-        {
-            
-        }
-
-        public void LoadData()
-        {
-
-        }
-        /// <summary>
-        /// Return Device Name or Monitor type by reading monitor number from hrm file
+        /// GetPolarDeviceName from its monitor no for e.g. if monitor=1 then device name = "Polar Sport Tester / Vantage"
         /// </summary>
         /// <param name="monitor"></param>
         /// <returns></returns>
