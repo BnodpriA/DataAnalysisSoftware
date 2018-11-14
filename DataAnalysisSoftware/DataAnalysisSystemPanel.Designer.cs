ea1d8380fea1d602eab35db0a77f237f505d65ae
[Binod Chaudhary] <[bunnycdy@gmail.com]>
2018-11-14 12:50:13 +0545

    Completed all the design work of main forn

--- DataAnalysisSoftware/DataAnalysisSystemPanel.Designer.cs
+++ DataAnalysisSoftware/DataAnalysisSystemPanel.Designer.cs
@@ -36,25 +36,47 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.btnLoadData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lklFacebook = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlHeader.SuspendLayout();
+            this.pnlFooter.SuspendLayout();
             this.pnlMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlHeader
             // 
             this.pnlHeader.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlHeader.Controls.Add(this.pictureBox3);
+            this.pnlHeader.Controls.Add(this.pictureBox2);
+            this.pnlHeader.Controls.Add(this.label3);
+            this.pnlHeader.Controls.Add(this.pictureBox1);
+            this.pnlHeader.Controls.Add(this.label2);
             this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(1370, 100);
+            this.pnlHeader.Size = new System.Drawing.Size(1103, 95);
             this.pnlHeader.TabIndex = 0;
             // 
             // pnlFooter
             // 
             this.pnlFooter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlFooter.Controls.Add(this.linkLabel1);
+            this.pnlFooter.Controls.Add(this.lklFacebook);
+            this.pnlFooter.Controls.Add(this.label1);
             this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlFooter.Location = new System.Drawing.Point(0, 607);
+            this.pnlFooter.Location = new System.Drawing.Point(0, 619);
             this.pnlFooter.Name = "pnlFooter";
-            this.pnlFooter.Size = new System.Drawing.Size(1370, 100);
+            this.pnlFooter.Size = new System.Drawing.Size(1103, 88);
             this.pnlFooter.TabIndex = 1;
             // 
             // pnlMenu
@@ -66,9 +88,9 @@
             this.pnlMenu.Controls.Add(this.btnChart);
             this.pnlMenu.Controls.Add(this.btnDataSummary);
             this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnlMenu.Location = new System.Drawing.Point(0, 100);
+            this.pnlMenu.Location = new System.Drawing.Point(0, 95);
             this.pnlMenu.Name = "pnlMenu";
-            this.pnlMenu.Size = new System.Drawing.Size(199, 507);
+            this.pnlMenu.Size = new System.Drawing.Size(199, 524);
             this.pnlMenu.TabIndex = 2;
             // 
             // btnDataSummary
@@ -121,12 +143,108 @@
             this.btnLoadData.Text = "Load Data";
             this.btnLoadData.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(398, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Developed and Copyright owned By:";
+            // 
+            // lklFacebook
+            // 
+            this.lklFacebook.AutoSize = true;
+            this.lklFacebook.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lklFacebook.LinkColor = System.Drawing.Color.Blue;
+            this.lklFacebook.Location = new System.Drawing.Point(401, 35);
+            this.lklFacebook.Name = "lklFacebook";
+            this.lklFacebook.Size = new System.Drawing.Size(319, 19);
+            this.lklFacebook.TabIndex = 0;
+            this.lklFacebook.TabStop = true;
+            this.lklFacebook.Text = "Binod Chaudhary (Student No: 77193460)";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.Location = new System.Drawing.Point(384, 61);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(353, 19);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "B.Chaudhary5248@student.leedsbeckett.ac.uk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(319, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(427, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "POLAR HRM2 DATA ANALYSIS SOFTWARE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(384, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(290, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Advance Software Engineering Project";
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.BackColor = System.Drawing.Color.White;
+            this.pnlContent.BackgroundImage = global::DataAnalysisSoftware.Properties.Resources._130;
+            this.pnlContent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(199, 95);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(904, 524);
+            this.pnlContent.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::DataAnalysisSoftware.Properties.Resources.tbc_logo;
+            this.pictureBox3.Location = new System.Drawing.Point(796, 1);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(161, 88);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DataAnalysisSoftware.Properties.Resources.leedsbeckett;
+            this.pictureBox2.Location = new System.Drawing.Point(961, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(138, 88);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DataAnalysisSoftware.Properties.Resources.polar_hrm;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // DataAnalysisSystemPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1370, 707);
+            this.ClientSize = new System.Drawing.Size(1103, 707);
+            this.Controls.Add(this.pnlContent);
             this.Controls.Add(this.pnlMenu);
             this.Controls.Add(this.pnlFooter);
             this.Controls.Add(this.pnlHeader);
@@ -135,7 +253,14 @@
             this.Name = "DataAnalysisSystemPanel";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.DataAnalysisSystemPanel_Load);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlFooter.ResumeLayout(false);
+            this.pnlFooter.PerformLayout();
             this.pnlMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -150,5 +275,14 @@
         private System.Windows.Forms.Button btnChart;
         private System.Windows.Forms.Button btnDataSummary;
         private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel lklFacebook;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
